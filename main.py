b7b68b66359b416e0a6eea26215467a4594dea14
Kevin Dixson <kdixson538@gmail.com>
2021-02-08 20:08:52 -0800

    CIPHAHHHH

--- main.py
+++ main.py
@@ -1,17 +1,39 @@
 import sys
+import os
+import io
+import secrets
+import numpy as np
 from Crypto.Cipher import AES
+from Crypto.Util.Padding import pad
+from PIL import Image
+
 
 def main():
+    img = Image.open('/Users/krdixson/Desktop/321/BlockCipher/cp-logo.bmp')
+    img_bytes = io.BytesIO()
+    img.save(img_bytes, format="BMP")
+    img_bytes = img_bytes.getvalue()
 
-    data = b"1234567nnn890123456"
-    key = b"Sixteen byte key"
+    key = makeKey()
+    print("key:", key.hex())
 
-    cipher = AES.new(key, AES.MODE_EAX)
-    ciphertext, tag = cipher.encrypt_and_digest(data)
+    cipher = AES.new(key, AES.MODE_ECB)
+    ciphertext_bytes = cipher.encrypt(pad(img_bytes, AES.block_size))
 
-    print("\ncipher:", ciphertext, "\n")
+    f = open("ciphahhh.txt", "wb")
+    f.write(ciphertext_bytes)
+    f.close()
+
+    # dont do this ----> print("\ncipher:", ciphertext_bytes.hex(), "\n")
 
     # https://pycryptodome.readthedocs.io/en/latest/src/cipher/aes.html
+
+
+def makeKey():
+    # returns 16 random bytes
+    # https://docs.python.org/3/library/secrets.html#module-secrets
+    return secrets.token_bytes(16)
+
 
 if __name__ == '__main__':
     main()
