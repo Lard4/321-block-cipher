3c4ef3ed093eca9112f86dec55fcae624efed30e
Kevin Dixson <kdixson538@gmail.com>
2021-03-01 19:31:09 -0800

    added documentation & cbc decryption

--- main.py
+++ main.py
@@ -14,6 +14,99 @@ purple = "\x1B[35m"
 end = "\x1B[0m"
 
 
+def make_key():
+    # returns 16 random bytes
+    # https://docs.python.org/3/library/secrets.html#module-secrets
+
+    # uncomment this line to make it not random:
+    # return bytes.fromhex("65f3028ab7b10f8f3967cdd721120df2")
+
+    return secrets.token_bytes(16)
+
+
+def make_iv():
+    # returns 16 random bytes
+    # https://docs.python.org/3/library/secrets.html#module-secrets
+    
+    # uncomment this line to make it not random:
+    # return bytes.fromhex("4493735b6a50dffc7e0d04bd751c2927")
+    
+    return secrets.token_bytes(16)
+
+
+def submit(input_val):
+    global global_key, global_iv
+
+    # URL encode ; to %3B and = to %3D
+    input_val = input_val.replace(";", "%3B")
+    input_val = input_val.replace("=", "%3D")
+
+    # put other stuff
+    input_val = "userid=456;userdata=" + input_val + ";session-id=31337"
+
+    # change to byte array
+    input_val = bytes(input_val, encoding="utf-8")
+
+    # pad the string
+    input_val = pad(input_val, 16)
+
+    # encrypt using CBC and return
+    encrypted_ciphertext = custom_cbc_encrypt(input_val, global_key, global_iv)
+    return encrypted_ciphertext
+
+
+def verify(input_val):
+    d = cbc_decrypt(input_val)
+    if (d.find(";admin=true;") == -1):
+        return False
+    else:
+        return True
+
+
+def testTaskI():
+    global global_key, global_iv
+
+    """ ECB encryption"""
+    # open image
+    img = Image.open('/Users/krdixson/Desktop/321/BlockCipher/cp-logo.bmp')
+
+    # convert to byte stream
+    img_hdr, img_bytes = bmp_to_bytes(img)
+
+    print(blue, "ECB key:", global_key.hex(), end)
+
+    # encrypt
+    ciphertext_bytes = custom_ecb_encrypt(img_bytes, global_key)
+
+    f = open("ECB_custom.bmp", "wb")
+    f.write(img_hdr)
+    f.write(ciphertext_bytes)
+    f.close()
+    
+    """ CBC encryption"""
+    # open image
+    img = Image.open('/Users/krdixson/Desktop/321/BlockCipher/cp-logo.bmp')
+
+    # convert to byte stream
+    img_hdr, img_bytes = bmp_to_bytes(img)
+
+    print(blue, "CBC key:", global_key.hex(), end)
+    print(purple, "CBC IV:", global_iv.hex(), end)
+
+    # encrypt
+    ciphertext_bytes = custom_cbc_encrypt(img_bytes, global_key, global_iv)
+
+    f = open("CBC_custom.bmp", "wb")
+    f.write(img_hdr)
+    f.write(ciphertext_bytes)
+    f.close()
+
+
+def testTaskII():
+    submit("You're the man now, dog")
+    submit("You're the man now; =, ; = dog")
+    
+
 def bmp_to_bytes(img):
     # convert to byte stream
     img_bytes = io.BytesIO()
@@ -31,147 +124,94 @@ def xor_byte_arrays(one, two):
     return bytes(a ^ b for (a, b) in zip(one, two))
 
 
-def custom_cbc():
-    # open image
-    img = Image.open('/Users/krdixson/Desktop/321/BlockCipher/cp-logo.bmp')
-    #img = Image.open('/Users/cameronpriest/Desktop/Winter Quarter/CPE 321/Assignments/blockciphers/cp-logo.bmp')
-
-    # convert to byte stream
-    img_hdr, img_bytes = bmp_to_bytes(img)
-
-    key = make_key()
-    print("key:", key.hex())
-
-    iv = make_iv()
-    print("IV:", iv.hex())
-
+def custom_cbc_encrypt(data, key, iv):
     cipher = AES.new(key, AES.MODE_ECB)
-    xor_blk = xor_byte_arrays(img_bytes[0:16], iv)
-        
+    xor_blk = xor_byte_arrays(data[0:16], iv)
+
     encrypted_block = cipher.encrypt(xor_blk)
     ciphertext_bytes = encrypted_block
     previous_block = encrypted_block
 
-    num_iters = (len(img_bytes) // 16) * 16
+    num_iters = (len(data) // 16) * 16
 
     for i in range(16, num_iters, 16):
-        block = img_bytes[i: i + 16]
+        block = data[i: i + 16]
         xord_block = xor_byte_arrays(previous_block, block)
         encrypted = cipher.encrypt(xord_block)
         ciphertext_bytes += encrypted
         previous_block = encrypted
 
-    if len(img_bytes) % 16 != 0:
-        block = pad(img_bytes[num_iters:], 16)
+    if len(data) % 16 != 0:
+        block = pad(data[num_iters:], 16)
         xord_block = xor_byte_arrays(previous_block, block)
         encrypted = cipher.encrypt(xord_block)
         ciphertext_bytes += encrypted
 
-    f = open("CBC_custom.bmp", "wb")
-    f.write(img_hdr)
-    f.write(ciphertext_bytes)
-    f.close()
+    return ciphertext_bytes
 
-def custom_ecb():
-    # open image
-    img = Image.open('/Users/krdixson/Desktop/321/BlockCipher/cp-logo.bmp')
-    #img = Image.open('/Users/cameronpriest/Desktop/Winter Quarter/CPE 321/Assignments/blockciphers/cp-logo.bmp')
-    
-    # convert to byte stream
-    img_hdr, img_bytes = bmp_to_bytes(img)
 
-    key = make_key()
-    print("key:", key.hex())
-
+def custom_cbc_decrypt(data, key, iv):
     cipher = AES.new(key, AES.MODE_ECB)
-    ciphertext_bytes = cipher.encrypt(img_bytes[0:16])
 
-    print("length =", len(img_bytes))
-    print("iteration max=", (len(img_bytes) // 16) * 16)
+    encrypted_block = cipher.decrypt(data[0:16])
+    plaintext_block = xor_byte_arrays(encrypted_block, iv)
 
-    num_iters = (len(img_bytes) // 16) * 16
+    plaintext_bytes = plaintext_block
+    previous_block = data[0:16]
+
+    num_iters = (len(data) // 16) * 16
 
     for i in range(16, num_iters, 16):
-        block = img_bytes[i:i+16]
+        block = data[i: i + 16]
+        decrypted = cipher.decrypt(block)
+        plaintext_block = xor_byte_arrays(decrypted, previous_block)
+        plaintext_bytes += plaintext_block
+        previous_block = block
+
+    if len(data) % 16 != 0:
+        block = data[num_iters:]
+        decrypted = cipher.decrypt(block)
+        plaintext_block = xor_byte_arrays(decrypted, previous_block)
+        plaintext_bytes += plaintext_block
+
+    return plaintext_bytes
+
+
+def custom_ecb_encrypt(data, key):
+    cipher = AES.new(key, AES.MODE_ECB)
+    ciphertext_bytes = cipher.encrypt(data[0:16])
+
+    num_iters = (len(data) // 16) * 16
+
+    for i in range(16, num_iters, 16):
+        block = data[i:i+16]
         ciphertext_bytes += cipher.encrypt(block)
 
-    if len(img_bytes) % 16 != 0:
-        ciphertext_bytes += cipher.encrypt(pad(img_bytes[num_iters:], 16))
-        print(len(ciphertext_bytes))
+    if len(data) % 16 != 0:
+        ciphertext_bytes += cipher.encrypt(pad(data[num_iters:], 16))
 
-    f = open("ECB_custom.bmp", "wb")
-    f.write(img_hdr)
-    f.write(ciphertext_bytes)
-    
-    f.close()
+    return ciphertext_bytes
 
 
-def correct_ecb():
-    # open image
-    img = Image.open('/Users/krdixson/Desktop/321/BlockCipher/cp-logo.bmp')
-    #img = Image.open('/Users/cameronpriest/Desktop/Winter Quarter/CPE 321/Assignments/blockciphers/cp-logo.bmp')
-
-    # convert to byte stream
-    img_hdr, img_bytes = bmp_to_bytes(img)
-
-    key = make_key()
-    print("key:", key.hex())
-
+def correct_ecb_encrypt(data, key):
     cipher = AES.new(key, AES.MODE_ECB)
-    ciphertext_bytes = cipher.encrypt(pad(img_bytes, 16))
-
-    f = open("ECB_correct.bmp", "wb")
-    f.write(img_hdr)
-    f.write(ciphertext_bytes)
-    f.close()
+    ciphertext_bytes = cipher.encrypt(pad(data, 16))
+    return ciphertext_bytes
 
 
-def correct_cbc():
-    # open image
-    img = Image.open('/Users/krdixson/Desktop/321/BlockCipher/cp-logo.bmp')
-    # img = Image.open('/Users/cameronpriest/Desktop/Winter Quarter/CPE 321/Assignments/blockciphers/cp-logo.bmp')
-
-    # convert to byte stream
-    img_hdr, img_bytes = bmp_to_bytes(img)
-
-    key = make_key()
-    print("key:", key.hex())
-
+def correct_cbc_encrypt(data, key):
     cipher = AES.new(key, AES.MODE_CBC)
     ciphertext_bytes = cipher.encrypt(pad(img_bytes, 16))
-
-    f = open("CBC_correct.bmp", "wb")
-    f.write(img_hdr)
-    f.write(ciphertext_bytes)
-    f.close()
-
-
-def make_key():
-    # returns 16 random bytes
-    # https://docs.python.org/3/library/secrets.html#module-secrets
-
-    # uncomment this line to make it not random:
-    return bytes.fromhex("65f3028ab7b10f8f3967cdd721120df2")
-
-    return secrets.token_bytes(16)
-
-def make_iv():
-    # returns 16 random bytes
-    # https://docs.python.org/3/library/secrets.html#module-secrets
-    
-    # uncomment this line to make it not random:
-    return bytes.fromhex("4493735b6a50dffc7e0d04bd751c2927")
-    
-    return secrets.token_bytes(16)
+    return ciphertext_bytes
 
 
 if __name__ == '__main__':
-    # ECB
-    # custom_ecb()
-    # correct_ecb()
-    
-    # CBC
-    custom_cbc()
-    correct_cbc()
+    global_key = make_key()
+    global_iv = make_iv()
 
+    # testTaskI()
+    # testTaskII()
 
+    ciph = custom_cbc_encrypt(bytes("hello cameron this is funny because camaron in spanish is shrimp lolololololol", encoding="utf-8"), global_key, global_iv)
+    plain = custom_cbc_decrypt(ciph, global_key, global_iv)
+    print(plain)
