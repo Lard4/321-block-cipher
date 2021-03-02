f729624d4d4b192f0e896cd54e3941abc75e04a1
Cameron Priest <38331483+cameronapriest@users.noreply.github.com>
2021-03-01 17:39:11 -0800

    cbc not working

--- main.py
+++ main.py
@@ -5,6 +5,14 @@ from Crypto.Util.Padding import pad
 from PIL import Image
 
 
+red = "\x1B[31m"
+green = "\x1B[32m"
+blue = "\x1B[34m"
+yellow = "\x1B[33m"
+purple = "\x1B[35m"
+end = "\x1B[0m"
+
+
 def bmp_to_bytes(img):
     # convert to byte stream
     img_bytes = io.BytesIO()
@@ -18,18 +26,63 @@ def bmp_to_bytes(img):
     return img_hdr, img_bytes
 
 
+def xor_iv_with_input(key, iv, img_bytes, start, end):
+    for index, byte in enumerate(img_bytes[start:end]):
+        res = byte ^ iv[index]
+        img_bytes[index] = res
+
+    return img_bytes[start:end]
+
+
 def custom_cbc():
     # open image
-    img = Image.open('/Users/krdixson/Desktop/321/BlockCipher/cp-logo.bmp')
+    # img = Image.open('/Users/krdixson/Desktop/321/BlockCipher/cp-logo.bmp')
+    img = Image.open('/Users/cameronpriest/Desktop/Winter Quarter/CPE 321/Assignments/blockciphers/cp-logo.bmp')
 
     # convert to byte stream
     img_hdr, img_bytes = bmp_to_bytes(img)
+
+    # make the byte array mutable
+    img_bytes = bytearray(img_bytes)
 
     key = make_key()
     print("key:", key.hex())
 
     iv = make_key()
     print("IV:", iv.hex())
+
+    cipher = AES.new(key, AES.MODE_ECB)
+    img_bytes[0:16] = xor_iv_with_input(key, iv, img_bytes, 0, 16)
+    encrypted_block = cipher.encrypt(img_bytes[0:16])
+    ciphertext_bytes = encrypted_block
+
+    """previous_block = encrypted_block
+    for i in range(16, len(img_bytes) // 16, 16):
+        block = img_bytes[i:i+16]
+        xord_block = xor_iv_with_input(key, previous_block, img_bytes, i, i+16)
+        encrypted = cipher.encrypt(xord_block)
+        ciphertext_bytes += encrypted
+        previous_block = encrypted"""
+
+    """if len(img_bytes) % 16 != 0:
+        ciphertext_bytes += cipher.encrypt(
+            pad(img_bytes[len(img_bytes)//16+1:], 16))"""
+
+    f = open("CBC_custom.bmp", "wb")
+    f.write(img_hdr)
+    f.write(ciphertext_bytes)
+    f.close()
+
+def custom_ecb():
+    # open image
+    # img = Image.open('/Users/krdixson/Desktop/321/BlockCipher/cp-logo.bmp')
+    img = Image.open('/Users/cameronpriest/Desktop/Winter Quarter/CPE 321/Assignments/blockciphers/cp-logo.bmp')
+    
+    # convert to byte stream
+    img_hdr, img_bytes = bmp_to_bytes(img)
+
+    key = make_key()
+    print("key:", key.hex())
 
     cipher = AES.new(key, AES.MODE_ECB)
     ciphertext_bytes = cipher.encrypt(img_bytes[0:16])
@@ -40,34 +93,7 @@ def custom_cbc():
 
     if len(img_bytes) % 16 != 0:
         ciphertext_bytes += cipher.encrypt(
-            pad(img_bytes[len(img_bytes)//16+1:], 16))
-
-    f = open("CBC_custom.bmp", "wb")
-    f.write(img_hdr)
-    f.write(ciphertext_bytes)
-    f.close()
-
-def custom_ecb():
-    # open image
-    img = Image.open('/Users/krdixson/Desktop/321/BlockCipher/cp-logo.bmp')
-    
-    # convert to byte stream
-    img_hdr, img_bytes = bmp_to_bytes(img)
-
-    key = make_key()
-    print("key:", key.hex())
-
-    cipher = AES.new(key, AES.MODE_ECB)
-    
-    # encrypt first block of 16 Bytes
-    ciphertext_bytes = cipher.encrypt(img_bytes[0:16])
-
-    for i in range(16, len(img_bytes) // 16, 16):
-        block = img_bytes[i:i+16]
-        ciphertext_bytes += cipher.encrypt(block)
-
-    if len(img_bytes) % 16 != 0:
-        ciphertext_bytes += cipher.encrypt(pad(img_bytes[len(img_bytes)//16+1 : ], 16))
+            pad(img_bytes[len(img_bytes)//16+1 : ], 16))
 
     f = open("ECB_custom.bmp", "wb")
     f.write(img_hdr)
@@ -77,7 +103,8 @@ def custom_ecb():
 
 def correct_ecb():
     # open image
-    img = Image.open('/Users/krdixson/Desktop/321/BlockCipher/cp-logo.bmp')
+    # img = Image.open('/Users/krdixson/Desktop/321/BlockCipher/cp-logo.bmp')
+    img = Image.open('/Users/cameronpriest/Desktop/Winter Quarter/CPE 321/Assignments/blockciphers/cp-logo.bmp')
 
     # convert to byte stream
     img_hdr, img_bytes = bmp_to_bytes(img)
@@ -96,7 +123,8 @@ def correct_ecb():
 
 def correct_cbc():
     # open image
-    img = Image.open('/Users/krdixson/Desktop/321/BlockCipher/cp-logo.bmp')
+    # img = Image.open('/Users/krdixson/Desktop/321/BlockCipher/cp-logo.bmp')
+    img = Image.open('/Users/cameronpriest/Desktop/Winter Quarter/CPE 321/Assignments/blockciphers/cp-logo.bmp')
 
     # convert to byte stream
     img_hdr, img_bytes = bmp_to_bytes(img)
@@ -125,10 +153,11 @@ def make_key():
 
 if __name__ == '__main__':
     # ECB
-    custom_ecb()
-    correct_ecb()
+    # custom_ecb()
+    # correct_ecb()
     
     # CBC
-    #custom_cbc()
-    #correct_cbc()
+    custom_cbc()
+    correct_cbc()
+
 
