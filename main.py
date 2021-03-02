9e30c3af63dddfec17a5b26fe1abad585577b198
Cameron Priest <cpriest@calpoly.edu>
2021-03-02 13:36:31 -0800

    more output formatting, verify() fixed

--- main.py
+++ main.py
@@ -32,112 +32,6 @@ def make_iv():
     # return bytes.fromhex("4493735b6a50dffc7e0d04bd751c2927")
     
     return secrets.token_bytes(16)
-
-
-def submit(input_val):
-    global global_key, global_iv
-
-    # URL encode ; to %3B and = to %3D
-    input_val = input_val.replace(";", "%3B")
-    input_val = input_val.replace("=", "%3D")
-
-    # put other stuff
-    input_val = "userid=456;userdata=" + input_val + ";session-id=31337"
-
-    # change to byte array
-    input_val = bytes(input_val, encoding="utf-8")
-
-    # pad the string
-    input_val = pkcs7(input_val, 16)
-
-    # encrypt using CBC and return
-    encrypted_ciphertext = custom_cbc_encrypt(input_val, global_key, global_iv)
-    return encrypted_ciphertext
-
-
-def verify(input_val):
-    global global_key, global_iv
-
-    d = custom_cbc_decrypt(input_val, global_key, global_iv)
-    if (d.find(";admin=true;") == -1):
-        return False
-    else:
-        return True
-
-
-def testTaskI():
-    global global_key, global_iv
-
-    print("Task I:")
-
-    """ ECB encryption"""
-    # open image
-    # img = Image.open('/Users/krdixson/Desktop/321/BlockCipher/cp-logo.bmp')
-    img = Image.open('/Users/cameronpriest/Desktop/Winter Quarter/CPE 321/Assignments/321-block-cipher/cp-logo.bmp')
-
-    # convert to byte stream
-    img_hdr, img_bytes = bmp_to_bytes(img)
-
-    print(blue, "ECB key:", global_key.hex(), end)
-
-    # encrypt
-    ciphertext_bytes = custom_ecb_encrypt(img_bytes, global_key)
-
-    f = open("ECB_custom.bmp", "wb")
-    f.write(img_hdr)
-    f.write(ciphertext_bytes)
-    f.close()
-
-    correct_ecb = correct_ecb_encrypt(img_bytes, global_key)
-    f = open("ECB_correct.bmp", "wb")
-    f.write(img_hdr)
-    f.write(correct_ecb)
-    f.close()
-
-    print(green, "diff ECB_correct.bmp ECB_custom.bmp", end="")
-    print(end, "will not differ\n")
-    
-    """ CBC encryption"""
-    # open image
-    # img = Image.open('/Users/krdixson/Desktop/321/BlockCipher/cp-logo.bmp')
-    img = Image.open('/Users/cameronpriest/Desktop/Winter Quarter/CPE 321/Assignments/321-block-cipher/cp-logo.bmp')
-
-    # convert to byte stream
-    img_hdr, img_bytes = bmp_to_bytes(img)
-
-    print(blue, "CBC key:", global_key.hex(), end)
-    print(blue, "CBC IV:", global_iv.hex(), end)
-
-    # encrypt
-    ciphertext_bytes = custom_cbc_encrypt(img_bytes, global_key, global_iv)
-
-    f = open("CBC_custom.bmp", "wb")
-    f.write(img_hdr)
-    f.write(ciphertext_bytes)
-    f.close()
-
-    correct_cbc = correct_cbc_encrypt(img_bytes, global_key)
-    f = open("CBC_correct.bmp", "wb")
-    f.write(img_hdr)
-    f.write(correct_cbc)
-    f.close()
-
-    print(purple, "diff CBC_correct.bmp CBC_custom.bmp", end="")
-    print(end, "will likely differ because the IV is random for CBC_correct.bmp\n")
-
-
-def testTaskII():
-    print("Task II:")
-
-    cipher = submit("You're the man now; =, ; = dog")
-    print(red, "ciphertext:", cipher, end)
-
-    # result = verify(cipher)
-    # print("verify() returned", result)
-
-    ciph = custom_cbc_encrypt(bytes("hello cameron this is funny because camaron in spanish is shrimp lolololololol", encoding="utf-8"), global_key, global_iv)
-    plain = custom_cbc_decrypt(ciph, global_key, global_iv)
-    print(plain)
     
 
 def bmp_to_bytes(img):
@@ -151,6 +45,19 @@ def bmp_to_bytes(img):
     img_bytes = img_bytes[54:]
 
     return img_hdr, img_bytes
+
+
+def pkcs7(block, blocksize):
+    # determine number of bytes to pad
+    padBytes = blocksize - len(block)
+    if padBytes == 0:
+        padBytes = blocksize
+
+    # pad the byte representation of the number of pad bytes until block filled
+    for i in range(padBytes):
+        block += bytes([padBytes])
+
+    return block
 
 
 def xor_byte_arrays(one, two):
@@ -238,16 +145,120 @@ def correct_cbc_encrypt(data, key):
     return ciphertext_bytes
 
 
-def pkcs7(block, blocksize):
-    padBytes = blocksize - len(block)
+def submit(input_val):
+    global global_key, global_iv
 
-    if padBytes == 0:
-        padBytes = blocksize
+    # URL encode ; to %3B and = to %3D
+    input_val = input_val.replace(";", "%3B")
+    input_val = input_val.replace("=", "%3D")
 
-    for i in range(padBytes):
-        block += bytes([padBytes])
+    # put other stuff
+    input_val = "userid=456;userdata=" + input_val + ";session-id=31337"
 
-    return block
+    # change to byte array
+    input_val = bytes(input_val, encoding="utf-8")
+
+    # pad the string
+    input_val = pkcs7(input_val, 16)
+
+    # encrypt using CBC and return
+    encrypted_ciphertext = custom_cbc_encrypt(input_val, global_key, global_iv)
+    return encrypted_ciphertext
+
+
+def verify(input_val):
+    global global_key, global_iv
+
+    d = custom_cbc_decrypt(input_val, global_key, global_iv)
+    d = d.decode("utf-8")
+
+    if (d.find(";admin=true;") == -1):
+        return False
+    else:
+        return True
+
+
+def testTaskI():
+    global global_key, global_iv
+
+    print(blue, end="")
+    print("Task I:", end)
+
+    """ ECB encryption"""
+    # open image
+    # img = Image.open('/Users/krdixson/Desktop/321/BlockCipher/cp-logo.bmp')
+    img = Image.open('/Users/cameronpriest/Desktop/Winter Quarter/CPE 321/Assignments/321-block-cipher/cp-logo.bmp')
+
+    # convert to byte stream
+    img_hdr, img_bytes = bmp_to_bytes(img)
+
+    print(" ECB key:", global_key.hex())
+
+    # encrypt
+    ciphertext_bytes = custom_ecb_encrypt(img_bytes, global_key)
+
+    f = open("ECB_custom.bmp", "wb")
+    f.write(img_hdr)
+    f.write(ciphertext_bytes)
+    f.close()
+
+    correct_ecb = correct_ecb_encrypt(img_bytes, global_key)
+    f = open("ECB_correct.bmp", "wb")
+    f.write(img_hdr)
+    f.write(correct_ecb)
+    f.close()
+
+    print(green, "diff ECB_correct.bmp ECB_custom.bmp", end="")
+    print(end, "will not differ\n")
+    
+    """ CBC encryption"""
+    # open image
+    # img = Image.open('/Users/krdixson/Desktop/321/BlockCipher/cp-logo.bmp')
+    img = Image.open('/Users/cameronpriest/Desktop/Winter Quarter/CPE 321/Assignments/321-block-cipher/cp-logo.bmp')
+
+    # convert to byte stream
+    img_hdr, img_bytes = bmp_to_bytes(img)
+
+    print(" CBC key:", global_key.hex())
+    print(" CBC IV:", global_iv.hex())
+
+    # encrypt
+    ciphertext_bytes = custom_cbc_encrypt(img_bytes, global_key, global_iv)
+
+    f = open("CBC_custom.bmp", "wb")
+    f.write(img_hdr)
+    f.write(ciphertext_bytes)
+    f.close()
+
+    correct_cbc = correct_cbc_encrypt(img_bytes, global_key)
+    f = open("CBC_correct.bmp", "wb")
+    f.write(img_hdr)
+    f.write(correct_cbc)
+    f.close()
+
+    print(yellow, "diff CBC_correct.bmp CBC_custom.bmp", end="")
+    print(end, "will likely differ because the IV is random for CBC_correct.bmp\n")
+
+
+def testTaskII():
+    print(blue, end="")
+    print("Task II:", end)
+
+    userString = input(" Please provide a string to encrypt: ")
+    cipher = submit("You're the man now; =, ; = dog")
+
+    result = verify(cipher)
+    print(" verify() returned ", end="")
+    if not result:
+        print(green, end="")
+    else:
+        print(red, end="")
+    print(result, end)
+    print()
+
+    ciph = custom_cbc_encrypt(bytes("hello cameron this is funny because camaron in spanish is shrimp lolololololol", encoding="utf-8"), global_key, global_iv)
+    plain = custom_cbc_decrypt(ciph, global_key, global_iv)
+    print(plain)
 
 
 if __name__ == '__main__':
